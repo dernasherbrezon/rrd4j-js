a9fcc634d57509cd6414a37d13647295adbf266b
dernasherbrezon <rodionovamp@mail.ru>
2017-11-17 08:46:15 +0000

    add eslint and fix formatting errors

--- src/index.js
+++ src/index.js
@@ -1,199 +1,220 @@
-function Datasource(rrdFile) {
-  this.dsName = rrdFile.getString();
-  this.dsType = rrdFile.getString();
-  this.heartbeat = rrdFile.getLong();
-  this.minValue = rrdFile.getDouble();
-  this.maxValue = rrdFile.getDouble();
-  this.lastValue = rrdFile.getDouble();
-  this.accumValue = rrdFile.getDouble();
-  this.nanSeconds = rrdFile.getLong();
+function Datasource (rrdFile) {
+  this.dsName = rrdFile.getString()
+  this.dsType = rrdFile.getString()
+  this.heartbeat = rrdFile.getLong()
+  this.minValue = rrdFile.getDouble()
+  this.maxValue = rrdFile.getDouble()
+  this.lastValue = rrdFile.getDouble()
+  this.accumValue = rrdFile.getDouble()
+  this.nanSeconds = rrdFile.getLong()
 }
 
-function Archive(rrdFile) {
-  this.consolFun = rrdFile.getString();
-  this.xff = rrdFile.getDouble();
-  this.steps = rrdFile.getInt();
-  this.rows = rrdFile.getInt();
-  this.headerStep = rrdFile.step;
-  this.headerLastUpdateTime = rrdFile.lastUpdateTime;
-  this.robins = [];
+function Archive (rrdFile) {
+  this.consolFun = rrdFile.getString()
+  this.xff = rrdFile.getDouble()
+  this.steps = rrdFile.getInt()
+  this.rows = rrdFile.getInt()
+  this.headerStep = rrdFile.step
+  this.headerLastUpdateTime = rrdFile.lastUpdateTime
+  this.robins = []
   for (var i = 0; i < rrdFile.dsCount; i++) {
-    var curPointer = rrdFile.getInt(), arcState = new ArcState(rrdFile);
-    this.robins.push(new RobinMatrix(curPointer, arcState, this.rows));
+    var curPointer = rrdFile.getInt()
+    var arcState = new ArcState(rrdFile)
+    this.robins.push(new RobinMatrix(curPointer, arcState, this.rows))
   }
   for (var j = 0; j < this.rows; j++) {
-    for (var i = 0; i < rrdFile.dsCount; i++) {
-      this.robins[i].data.push(rrdFile.getDouble());
+    for (i = 0; i < rrdFile.dsCount; i++) {
+      this.robins[i].data.push(rrdFile.getDouble())
     }
   }
 }
 
-Archive.prototype.getArcStep = function() {
-  return this.headerStep * this.steps;
+Archive.prototype.getArcStep = function () {
+  return this.headerStep * this.steps
 }
 
-Archive.prototype.getStartTime = function() {
-  var endTime = this.getEndTime(), arcStep = this.getArcStep();
-  return endTime - (this.rows - 1) * arcStep;
+Archive.prototype.getStartTime = function () {
+  var endTime = this.getEndTime()
+  var arcStep = this.getArcStep()
+  return endTime - (this.rows - 1) * arcStep
 }
 
-Archive.prototype.getEndTime = function() {
-  return normalize(this.headerLastUpdateTime, this.getArcStep());
+Archive.prototype.getEndTime = function () {
+  return normalize(this.headerLastUpdateTime, this.getArcStep())
 }
 
-Archive.prototype.getData = function(dsIndex, requestFetchStart, requestFetchEnd) {
-  var arcStep = this.getArcStep(), fetchStart = normalize(requestFetchStart, arcStep), fetchEnd = normalize(requestFetchEnd, arcStep);
+Archive.prototype.getData = function (dsIndex, requestFetchStart, requestFetchEnd) {
+  var arcStep = this.getArcStep()
+  var fetchStart = normalize(requestFetchStart, arcStep)
+  var fetchEnd = normalize(requestFetchEnd, arcStep)
   if (fetchEnd < requestFetchEnd) {
-    fetchEnd += arcStep;
+    fetchEnd += arcStep
   }
-  var startTime = this.getStartTime(), endTime = this.getEndTime(), ptsCount = ((fetchEnd - fetchStart) / arcStep + 1), matchStartTime = Math.max(fetchStart, startTime), matchEndTime = Math.min(fetchEnd, endTime), robinValues = [];
+  var startTime = this.getStartTime()
+  var endTime = this.getEndTime()
+  var ptsCount = ((fetchEnd - fetchStart) / arcStep + 1)
+  var matchStartTime = Math.max(fetchStart, startTime)
+  var matchEndTime = Math.min(fetchEnd, endTime)
+  var robinValues = []
 
   if (matchStartTime <= matchEndTime) {
     // preload robin values
-    var matchCount = ((matchEndTime - matchStartTime) / arcStep + 1), matchStartIndex = ((matchStartTime - startTime) / arcStep);
-    robinValues = this.robins[dsIndex].getValues(matchStartIndex, matchCount);
+    var matchCount = ((matchEndTime - matchStartTime) / arcStep + 1)
+    var matchStartIndex = ((matchStartTime - startTime) / arcStep)
+    robinValues = this.robins[dsIndex].getValues(matchStartIndex, matchCount)
   }
-  var result = [];
+  var result = []
   for (var ptIndex = 0; ptIndex < ptsCount; ptIndex++) {
-    var time = fetchStart + ptIndex * arcStep, value = NaN;
+    var time = fetchStart + ptIndex * arcStep
+    var value = NaN
     if (time >= matchStartTime && time <= matchEndTime) {
       // inbound time
-      value = robinValues[((time - matchStartTime) / arcStep)];
+      value = robinValues[((time - matchStartTime) / arcStep)]
     }
-    result.push([ (fetchStart + ptIndex * arcStep) * 1000, value ]);
+    result.push([ (fetchStart + ptIndex * arcStep) * 1000, value ])
   }
 
-  return result;
+  return result
 }
 
-function normalize(timestamp, step) {
-  return timestamp - timestamp % step;
+function normalize (timestamp, step) {
+  return timestamp - timestamp % step
 }
 
-function RobinMatrix(pointer, arcState, rows) {
-  this.pointer = pointer;
-  this.arcState = arcState;
-  this.rows = rows;
-  this.data = [];
+function RobinMatrix (pointer, arcState, rows) {
+  this.pointer = pointer
+  this.arcState = arcState
+  this.rows = rows
+  this.data = []
 }
 
-RobinMatrix.prototype.getValues = function(index, count) {
-  var startIndex = (this.pointer + index) % this.rows, tailReadCount = Math.min(this.rows - startIndex, count), tailValues = this.data.slice(startIndex, startIndex + tailReadCount);
+RobinMatrix.prototype.getValues = function (index, count) {
+  var startIndex = (this.pointer + index) % this.rows
+  var tailReadCount = Math.min(this.rows - startIndex, count)
+  var tailValues = this.data.slice(startIndex, startIndex + tailReadCount)
   if (tailReadCount < count) {
-    var headReadCount = count - tailReadCount, headValues = this.data.slice(0, 0 + headReadCount);
-    return tailValues.concat(headValues);
+    var headReadCount = count - tailReadCount
+    var headValues = this.data.slice(0, 0 + headReadCount)
+    return tailValues.concat(headValues)
   } else {
-    return tailValues;
+    return tailValues
   }
 }
 
-function ArcState(rrdFile) {
-  this.accumValue = rrdFile.getDouble();
-  this.nanSteps = rrdFile.getLong();
+function ArcState (rrdFile) {
+  this.accumValue = rrdFile.getDouble()
+  this.nanSteps = rrdFile.getLong()
 }
 
-function RRDFile(byteArray) {
-  this.data = byteArray;
-  this.index = 0;
-  this.signature = this.getString();
-  this.step = this.getLong();
-  this.dsCount = this.getInt();
-  this.arcCount = this.getInt();
-  this.lastUpdateTime = this.getLong();
-  this.datasources = [];
+function RRDFile (byteArray) {
+  this.data = byteArray
+  this.index = 0
+  this.signature = this.getString()
+  this.step = this.getLong()
+  this.dsCount = this.getInt()
+  this.arcCount = this.getInt()
+  this.lastUpdateTime = this.getLong()
+  this.datasources = []
   for (var i = 0; i < this.dsCount; i++) {
-    this.datasources.push(new Datasource(this));
+    this.datasources.push(new Datasource(this))
   }
-  this.archives = [];
-  for (var i = 0; i < this.arcCount; i++) {
-    this.archives.push(new Archive(this));
+  this.archives = []
+  for (i = 0; i < this.arcCount; i++) {
+    this.archives.push(new Archive(this))
   }
 }
 
-RRDFile.prototype.getData = function(dsName, consolFun, fetchStart, fetchEnd) {
-  var dsIndex = this.getDsIndex(dsName), archive = this.getArchive(consolFun, fetchStart / 1000, fetchEnd / 1000);
-  if (dsIndex == -1 || archive == null) {
-    return {};
+RRDFile.prototype.getData = function (dsName, consolFun, fetchStart, fetchEnd) {
+  var dsIndex = this.getDsIndex(dsName)
+  var archive = this.getArchive(consolFun, fetchStart / 1000, fetchEnd / 1000)
+  if (dsIndex === -1 || archive === null) {
+    return {}
   }
   var result = {
-    label : dsName,
-    data : archive.getData(dsIndex, fetchStart / 1000, fetchEnd / 1000)
-  };
-  return result;
+    label: dsName,
+    data: archive.getData(dsIndex, fetchStart / 1000, fetchEnd / 1000)
+  }
+  return result
 }
 
-RRDFile.prototype.getArchive = function(consolFun, fetchStart, fetchEnd) {
-  var bestFullMatch = null, bestPartialMatch = null, bestStepDiff = 0, bestMatch = 0;
+RRDFile.prototype.getArchive = function (consolFun, fetchStart, fetchEnd) {
+  var bestFullMatch = null
+  var bestPartialMatch = null
+  var bestStepDiff = 0
+  var bestMatch = 0
   for (var i = 0; i < this.arcCount; i++) {
-    var archive = this.archives[i];
-    if (archive.consolFun != consolFun) {
-      continue;
+    var archive = this.archives[i]
+    if (archive.consolFun !== consolFun) {
+      continue
     }
-    var arcStep = archive.getArcStep(), arcStart = archive.getStartTime() - arcStep, fullMatch = fetchEnd - fetchStart, tmpStepDiff = Math.abs(archive.getArcStep() - 1);
+    var arcStep = archive.getArcStep()
+    var arcStart = archive.getStartTime() - arcStep
+    var fullMatch = fetchEnd - fetchStart
+    var tmpStepDiff = Math.abs(archive.getArcStep() - 1)
     // we need step difference in either full or partial case
     if (arcStart <= fetchStart) {
       // best full match
       if (bestFullMatch == null || tmpStepDiff < bestStepDiff) {
-        bestStepDiff = tmpStepDiff;
-        bestFullMatch = archive;
+        bestStepDiff = tmpStepDiff
+        bestFullMatch = archive
       }
     } else {
       // best partial match
-      var tmpMatch = fullMatch;
+      var tmpMatch = fullMatch
       if (arcStart > fetchStart) {
-        tmpMatch -= (arcStart - fetchStart);
+        tmpMatch -= (arcStart - fetchStart)
       }
-      if (bestPartialMatch == null || bestMatch < tmpMatch || (bestMatch == tmpMatch && tmpStepDiff < bestStepDiff)) {
-        bestPartialMatch = archive;
-        bestMatch = tmpMatch;
+      if (bestPartialMatch === null || bestMatch < tmpMatch || (bestMatch === tmpMatch && tmpStepDiff < bestStepDiff)) {
+        bestPartialMatch = archive
+        bestMatch = tmpMatch
       }
     }
   }
   if (bestFullMatch != null) {
-    return bestFullMatch;
+    return bestFullMatch
   } else if (bestPartialMatch != null) {
-    return bestPartialMatch;
+    return bestPartialMatch
   }
-  return null;
+  return null
 }
 
-RRDFile.prototype.getDsIndex = function(dsName) {
+RRDFile.prototype.getDsIndex = function (dsName) {
   for (var i = 0; i < this.dsCount; i++) {
-    if (dsName == this.datasources[i].dsName) {
-      return i;
+    if (dsName === this.datasources[i].dsName) {
+      return i
     }
   }
-  return -1;
+  return -1
 }
 
-RRDFile.prototype.getString = function() {
-  var result = "";
+RRDFile.prototype.getString = function () {
+  var result = ''
   for (var i = 0; i < 40; i += 2) {
-    result += String.fromCharCode(this.data[this.index + i + 1]);
+    result += String.fromCharCode(this.data[this.index + i + 1])
   }
-  this.index += 40;
-  return result.trim();
+  this.index += 40
+  return result.trim()
 }
 
-RRDFile.prototype.getDouble = function() {
-  var view = new DataView(this.data.buffer, this.index, 8);
-  var result = view.getFloat64(0, false);
-  this.index += 8;
-  return result;
+RRDFile.prototype.getDouble = function () {
+  var view = new DataView(this.data.buffer, this.index, 8)
+  var result = view.getFloat64(0, false)
+  this.index += 8
+  return result
 }
 
-RRDFile.prototype.getLong = function() {
-  var high = this.getInt();
-  var low = this.getInt();
-  return (high << 32) + (low & 0xFFFFFFFF);
+RRDFile.prototype.getLong = function () {
+  var high = this.getInt()
+  var low = this.getInt()
+  return (high << 32) + (low & 0xFFFFFFFF)
 }
 
-RRDFile.prototype.getInt = function() {
-  var result = ((this.data[this.index + 0] << 24) & 0xFF000000) + ((this.data[this.index + 1] << 16) & 0x00FF0000) + ((this.data[this.index + 2] << 8) & 0x0000FF00) + ((this.data[this.index + 3] << 0) & 0x000000FF);
-  this.index += 4;
-  return result;
+RRDFile.prototype.getInt = function () {
+  var result = ((this.data[this.index + 0] << 24) & 0xFF000000) + ((this.data[this.index + 1] << 16) & 0x00FF0000) + ((this.data[this.index + 2] << 8) & 0x0000FF00) + ((this.data[this.index + 3] << 0) & 0x000000FF)
+  this.index += 4
+  return result
 }
 
-RRDFile.prototype.skip = function(numBytes) {
-  this.index += numBytes;
+RRDFile.prototype.skip = function (numBytes) {
+  this.index += numBytes
 }
