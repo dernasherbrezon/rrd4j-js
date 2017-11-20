2629f9de8cd1f8ed136741549e92c01b0a1b6885
dernasherbrezon <rodionovamp@mail.ru>
2017-11-20 09:31:07 +0000

    data test

--- test/test.js
+++ test/test.js
@@ -15,8 +15,9 @@ describe('rrdfile', function () {
   it('should return some data', function (done) {
     fs.readFile(path.join(__dirname, '/demo.rrd'), function (err, data) {
       expect(err).to.equal(null)
-      var result = new RRDFile(data).getData('sun', 'AVERAGE', new Date(), new Date())
-      expect(result).to.deep.equal({})
+      var result = new RRDFile(data).getData('sun', 'AVERAGE', new Date(1278111429000), new Date(1278111431000))
+      expect(result.label).to.equal('sun')
+      expect(result.data).to.deep.equal([[1278111300000, 3957.25], [1278111600000, NaN]])
       done()
     })
   })
