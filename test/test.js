7176f484ead03ff44b689b2d4c61f3b00859718a
dernasherbrezon <rodionovamp@mail.ru>
2017-11-20 08:54:00 +0000

    fix async test

--- test/test.js
+++ test/test.js
@@ -4,11 +4,20 @@ var fs = require('fs')
 var path = require('path')
 
 describe('rrdfile', function () {
-  it('should return empty array for unknown ds', function () {
+  it('should return empty array for unknown ds', function (done) {
     fs.readFile(path.join(__dirname, '/demo.rrd'), function (err, data) {
-      expect(err).to.be.null()
+      expect(err).to.equal(null)
       var result = new RRDFile(data).getData(Math.random().toString(36).substring(7), 'AVERAGE', new Date(), new Date())
-      expect(result).to.be.a({})
+      expect(result).to.deep.equal({})
+      done()
+    })
+  })
+  it('should return some data', function (done) {
+    fs.readFile(path.join(__dirname, '/demo.rrd'), function (err, data) {
+      expect(err).to.equal(null)
+      var result = new RRDFile(data).getData('sun', 'AVERAGE', new Date(), new Date())
+      expect(result).to.deep.equal({})
+      done()
     })
   })
 })
