1cfc6e9bf541c52abc09737c99f67aeeb97bca17
dernasherbrezon <rodionovamp@mail.ru>
2017-11-22 11:31:56 +0000

    remove unnecessary logging

--- test/test.js
+++ test/test.js
@@ -7,7 +7,6 @@ describe('rrdfile', function () {
   it('should return empty array for unknown ds', function (done) {
     fs.readFile(path.join(__dirname, '/demo.rrd'), function (err, data) {
       expect(err).to.equal(null)
-      console.log(RRDFile)
       var result = new RRDFile(data).getData(Math.random().toString(36).substring(7), 'AVERAGE', new Date(), new Date())
       expect(result).to.deep.equal({})
       done()
